0f06e7f3884341fe011b2702cb957aeeb1c60b89
SVA2021 <sofronov.vit@gmail.com>
2021-12-22 13:35:56 +0400

    mainBtn refactoring

---
+++ src/components/Sidebar/MainBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './MainBtn.module.scss'
+
+const MainBtn = (props) => {
+
+    return (
+        <button onClick={props.setMain} className={props.class}> {props.value} </button>
+    )
+}
+
+export default MainBtn;
--- src/components/Sidebar/MainBtnList.jsx
+++ src/components/Sidebar/MainBtnList.jsx
@@ -2,42 +2,43 @@ import React from 'react';
 import style from './MainBtnList.module.scss'
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import { resetOptions, selectMainOption, selectSlider, setMainOption } from '../Main/demoSlice.ts';
-
-const MainBtn = (props) => {
-
-    const active = useAppSelector(selectMainOption);
-    const dispatch = useAppDispatch();
-
-    const setMain = () => {
-        dispatch(setMainOption(props.value));
-    }
-
-    let classes = (active === props.value)
-        ? style.sidebar__optionItem__btn__active
-        : style.sidebar__optionItem__btn;
-
-    return (
-        <button onClick={setMain} className={classes}> {props.value} </button>
-    )
-}
+import MainBtn from './MainBtn';
 
 const MainBtnList = () => {
 
+    const active = useAppSelector(selectMainOption);
     const page = useAppSelector(selectSlider);
     const dispatch = useAppDispatch();
+
+    const mainBtnList = Object.keys(page);
+
+    const setMain = (value) => {
+        dispatch(setMainOption(value));
+    }
+
+    let isActive = (value) => {
+        if (active === value) {
+            return style.sidebar__optionItem__btn__active
+        } else {
+            return style.sidebar__optionItem__btn;
+        }
+    }
 
     const reset = () => {
         dispatch(resetOptions());
     }
 
-    const mainBtnList = Object.keys(page);
-
     return (
         <div className={style.sidebar__mainBtn} >
             <h2 className={style.sidebar__header}>Settings</h2>
             <button onClick={reset}>reset all</button>
-            <div className={style.sidebar__mainBtn__list} > 
-            {mainBtnList.map((item) => <MainBtn key={item} value={item} />)}
+            <div className={style.sidebar__mainBtn__list} >
+                {mainBtnList.map((item) => <MainBtn
+                    key={item}
+                    value={item}
+                    class={isActive(item)}
+                    setMain={() => setMain(item)}
+                />)}
             </div>
         </div>
     )
