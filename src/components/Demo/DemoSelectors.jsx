a79613893f7ec302c9baa803810f11650dd5fe07
SVA2021 <sofronov.vit@gmail.com>
2022-01-03 00:14:45 +0400

    demoselectors style updated

--- src/components/Demo/DemoSelectors.jsx
+++ src/components/Demo/DemoSelectors.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useAppSelector } from '../../app/hooks';
 import { selectDemo } from '../../app/demoSlice';
 import style from './DemoSelectors.module.scss'
@@ -11,44 +10,44 @@ const DemoSelectors = () => {
     const part1 = activeStyle.part1.elementAndClass;
 
     return (
-        <article className={"demo"}>
-            <header>
-                {/* <h4>Part 1. Selection by elements and class</h4> */}
-                <SectionTitle>CSS Selectors</SectionTitle>
-                <p>syntaxis in css file: {part1?.class}</p>
-                <p>description: {part1?.description}</p>
-            </header>
-
-            <div className={part1?.style}>
-                <ul className={style.main}>
-                    ul
-                    <li>li-1
-                        <p className='class1 class2'>p-1 class = name1 name2</p>
-                    </li>
-                    <li>li-2
-                        <p id='firstname'>p-2-1 id = firstname</p>
-                        <p>p-2-2</p>
-                    </li>
-                    <li className='class1'>li-3 class = name1
-                        <div>
-                            <p className='class2'>p-3 class = name2</p>
-                            div3
-                        </div>
-                    </li>
-                    <li>li-4
-                        <p>p-4-1</p>
-                        <span>span4</span>
-                        <p className={'intro'}>p-4-2 class = intro </p>
-                        <p lang='en'>p-4-3 lang=en</p>
-                        <p></p>
-                    </li>
-                    <li>li-5
-                        <div>div5</div>
-                        <p title='flower'>p-5 "\n /n"
-                            title=flower</p>
-                    </li>
-                    <p>external p</p>
-                    <ul>
+        <section className={"demo"}>
+            <SectionTitle>CSS Selectors</SectionTitle>
+            <div className={style.fix}>
+                <header>
+                    <p><b>syntaxis in css file: </b>{part1?.class}</p>
+                    <p><b>description: </b>{part1?.description}</p>
+                </header>
+                <div className={part1?.style}>
+                    <ul className={style.main}>
+                        ul
+                        <li>li-1
+                            <p className='class1 class2'>p-1 class = name1 name2</p>
+                        </li>
+                        <li>li-2
+                            <p id='firstname'>p-2-1 id = firstname</p>
+                            <p>p-2-2</p>
+                        </li>
+                        <li className='class1'>li-3 class = name1
+                            <div>
+                                <p className='class2'>p-3 class = name2</p>
+                                div3
+                            </div>
+                        </li>
+                        <li>li-4
+                            <p>p-4-1</p>
+                            <span>span4</span>
+                            <p className={'intro'}>p-4-2 class = intro </p>
+                            <p lang='en'>p-4-3 lang=en</p>
+                            <p></p>
+                        </li>
+                        <li>li-5
+                            <div>div5</div>
+                            <p title='flower'>p-5 "\n /n"
+                                title=flower</p>
+                        </li>
+                    </ul>
+                    <p className={style.additionP}>external p</p>
+                    <ul className={style.main}>
                         <li>
                             <a href="#00">special link #00</a>
                             <a href="#01w3schools.pdf" >special link #01</a>
@@ -65,9 +64,9 @@ const DemoSelectors = () => {
                         </li>
                         <li></li>
                     </ul>
-                </ul>
+                </div>
             </div>
-        </article>
+        </section>
     )
 }
 
