26b4da8229b059493449d289b2655bbd7ae0c727
SVA2021 <sofronov.vit@gmail.com>
2022-01-03 22:44:19 +0400

    grid added to db

--- src/app/data.js
+++ src/app/data.js
@@ -96,7 +96,7 @@ export const positionElem = {//pages
             ],
         },
         childItem: {
-            alignSelf : [
+            alignSelf: [
                 { id: 1, name: 'initial' },
                 { id: 2, name: 'stretch' },
                 { id: 3, name: 'center' },
@@ -104,21 +104,68 @@ export const positionElem = {//pages
                 { id: 5, name: 'flexEnd' },
                 { id: 6, name: 'baseline' },
                 { id: 7, name: 'inherit' }
-              ] ,
+            ],
         }
     },
     grid: { // add some specific features
         parent: {
+            display: [{ id: 1, name: 'grid' }],
+            justifyContent: [
+                { id: 1, name: 'initial' },
+                { id: 2, name: 'flexStart' },
+                { id: 3, name: 'flexEnd' },
+                { id: 4, name: 'center' },
+                { id: 5, name: 'spaceBetween' },
+                { id: 6, name: 'spaceAround' },
+                { id: 7, name: 'spaceEvenly' },
+                { id: 8, name: 'inherit' }
+            ],
+            alignContent: [
+                { id: 1, name: 'initial' },
+                { id: 2, name: 'stretch' },
+                { id: 3, name: 'center' },
+                { id: 4, name: 'flexStart' },
+                { id: 5, name: 'flexEnd' },
+                { id: 6, name: 'spaceBetween' },
+                { id: 7, name: 'spaceAround' },
+                { id: 8, name: 'inherit' }
+            ],
+            gridTemplateColumns: [
+                { id: 1, name: '100px 100px 100px' },
+                { id: 2, name: '1fr 1fr 1fr' },
+                { id: 3, name: '100px 200px 100px' },
+                { id: 4, name: '1fr 2fr 1fr' },
+                { id: 5, name: '20% 20% 20%' },
+                { id: 6, name: '20% 50% 20%' },
+                { id: 7, name: 'repeat(3, 1fr)' },
+            ],
+            gridAutoRows: [
+                { id: 1, name: '100px' },
+                { id: 2, name: '1fr' },
+                { id: 3, name: '20%' },
+                { id: 4, name: 'minmax(100px, auto)' },
+            ],
+            gridGap: [
+                { id: 1, name: '10px' },
+                { id: 2, name: '5%' },
+                { id: 3, name: 'revert' },
+                { id: 4, name: 'unset' },
+                { id: 5, name: 'inherit' },
+                { id: 6, name: 'initial' },
+            ],
         },
         childItem: {
+            alignSelf: [
+                { id: 1, name: 'initial' },
+                { id: 2, name: 'stretch' },
+                { id: 3, name: 'center' },
+                { id: 4, name: 'flexStart' },
+                { id: 5, name: 'flexEnd' },
+                { id: 6, name: 'baseline' },
+                { id: 7, name: 'inherit' }
+            ],
         }
     },
-    position: { // need to fill
-        parent: {
-        },
-        childItem: {
-        }
-    }
 }
 
 export const selectorsCSS = {
