8fc2f4632a9db1977c91ac57be102ac22df236b7
SVA2021 <sofronov.vit@gmail.com>
2022-01-06 01:29:00 +0400

    preloader info updated

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -5,6 +5,18 @@ const Preloader = () => {
     return (
         <section className={'preloader'}>
             <SectionTitle>preloader will be here</SectionTitle>
+            <p>
+                at this page was used :
+                react
+                react-spring
+                sass
+                css modules
+                git 
+                github
+                ts
+                redux
+                redux-toolkit
+            </p>
         </section>
     )
 }
