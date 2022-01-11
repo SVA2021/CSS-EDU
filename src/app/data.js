77c4456cbf0f97e2ab6930563a6dc8c2574f6847
SVA2021 <sofronov.vit@gmail.com>
2022-01-12 01:54:12 +0400

    gradient finished

--- src/app/data.js
+++ src/app/data.js
@@ -238,7 +238,7 @@ export const selectorsCSS = {
 }
 
 export const animations = {
-    AnimationSelectors: { // need to fill
+    AnimationSelectors: { 
         part1: {
             properties: [
                 {
@@ -376,14 +376,20 @@ export const features = {
                 { id: 8, name: 'opacity', min: 0, max: 100, unit: '%' },
                 { id: 9, name: 'saturate', min: 0, max: 200, unit: '%' },
                 { id: 10, name: 'sepia', min: 0, max: 100, unit: '%' },
-                // { id: 11, name: 'initial' },
-                // { id: 12, name: 'none' },
-                // { id: 13, name: 'inherit' },
             ],
-            // gradient: [
-            //     { id: 1, name: 'grayscale', min: 0, max: 100, unit: '%' },
-            // ],
         },
-    }
+    },
+    gradient: {
+        part1: {
+            gradient: [
+                { id: 1, name: 'linear-gradient', min: 0, max: 100, unit: '%' },
+                { id: 2, name: 'repeating-linear-gradient', min: 0, max: 100, unit: '%' },
+                { id: 3, name: 'radial-gradient', min: 0, max: 10, unit: 'px' },
+                { id: 4, name: 'repeating-radial-gradient', min: 0, max: 10, unit: 'px' },
+                { id: 5, name: 'conic-gradient', min: 0, max: 200, unit: '%' },
+                { id: 6, name: 'repeating-conic-gradient', min: 0, max: 200, unit: '%' },
+            ],
+        },
+    },
 
 };
---
+++ src/components/Demo/Gradient.jsx
@@ -0,0 +1,69 @@
+import { useAppSelector } from '../../app/hooks';
+import style from './Gradient.module.scss'
+import { selectDemo } from '../../app/demoSlice';
+import { Highlighted, SectionTitle, Strong } from '../common/Typography';
+import React, { useState } from 'react';
+import { normalizeObjText } from '../../app/functionStore';
+
+const Gradient = (props) => {
+    const activeStyle = useAppSelector(selectDemo);
+    const activeDemoStatus = activeStyle.part1.gradient;
+
+    const [angle, setAngle] = useState(6);
+    const [startColorPos, setStartColorPos] = useState(0);
+    const [endColorPos, setEndColorPos] = useState(10);
+    const [posX, setX] = useState(10);
+    const [posY, setY] = useState(10);
+
+    const activeOption = activeDemoStatus?.name;
+    let angleCSS = "";
+    if (activeDemoStatus.id < 3) angleCSS = String(angle * 15) + "deg , ";
+    if (activeDemoStatus.id > 4) angleCSS = "from " + String(angle * 15) + "deg ";
+    let positionXY = "";
+    if (activeDemoStatus.id > 2) positionXY = "at " + String(posX * 5) + "% " + String(posY * 5) + "%, ";
+    const startColorPositionCSS = String(startColorPos * 5) + "%";
+    const endColorPositionCSS = String(endColorPos * 5) + "%";
+    const gradientCSS = " " + activeOption + "( "
+        + angleCSS + positionXY
+        + "blue " + startColorPositionCSS + ", red " + endColorPositionCSS + ") ";
+
+    const currentStyle = { background: gradientCSS };
+    const currentStyleTexted = normalizeObjText(currentStyle);
+    return (
+        <section className={"demo"}>
+            <SectionTitle>Gradient</SectionTitle>
+            <div className={style.fix}>
+                <div className={style.form}>
+                    <fieldset>
+                        <legend><Strong>Direction</Strong></legend>
+                        <label htmlFor="angle"><Strong>set direction angle</Strong></label>
+                        <input type="range" min="0" max="24" value={angle} name="angle"
+                            onChange={(e) => setAngle(e.target.value)} />
+                    </fieldset>
+                    <fieldset>
+                        <legend><Strong>Color Positions</Strong></legend>
+                        <label htmlFor="startColorPos"><Strong>set startColor</Strong></label>
+                        <input type="range" min="0" max="20" value={startColorPos} name="startColorPos"
+                            onChange={(e) => setStartColorPos(e.target.value)} />
+                        <label htmlFor="endColorPos"><Strong>set endColor</Strong></label>
+                        <input type="range" min="0" max="20" value={endColorPos} name="endColorPos"
+                            onChange={(e) => setEndColorPos(e.target.value)} />
+                    </fieldset>
+                    <fieldset>
+                        <legend><Strong>Set XY Position</Strong></legend>
+                        <label htmlFor="x"><Strong>set X position</Strong></label>
+                        <input type="range" min="0" max="20" value={posX} name="x"
+                            onChange={(e) => setX(e.target.value)} />
+                        <label htmlFor="y"><Strong>set Y position</Strong></label>
+                        <input type="range" min="0" max="20" value={posY} name="y"
+                            onChange={(e) => setY(e.target.value)} />
+                    </fieldset>
+                </div>
+                <Highlighted><Strong>CSS: </Strong>{currentStyleTexted}</Highlighted>
+                <div className={style.main} style={currentStyle}></div>
+            </div>
+        </section>
+    )
+}
+
+export default Gradient;
